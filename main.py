5cb6b535179cfdf6daa533f71f319ede7c4d5e59
demiCatson <gustavo.hebling@usp.br>
2018-11-27 15:04:31 -0200

    melhorias gerais

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import nQueens
 #Criado em 27/11
 #Problema das N rainhas
 
-#Representacao LAGUNA: board = [c1, c2, c3, c4]
-#linha i => coluna board[i]
-
 
 ###############################
 for index in range(4,10):
--- nQueens.py
+++ nQueens.py
@@ -1,3 +1,9 @@
+#Criado em 27/11
+#Problema das N rainhas
+
+#Representacao LAGUNA: board = [c1, c2, c3, c4]
+#linha i => coluna board[i]
+
 from random import randint
 
 max_iter = 1000
